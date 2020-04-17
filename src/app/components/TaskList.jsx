777099e015f929f53ffee68ad73534d22345dce7
Jona Siri <jsiri@sherwoodsys.com>
2020-04-17 10:04:09 +0100

    finished front end

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
+import { requestTaskCreation } from "../store/mutations";
+import { Link } from "react-router-dom";
 
-export const TaskList = ({ tasks, name }) => (
+export const TaskList = ({ tasks, name, id, createNewTask }) => (
   <div>
     <h3>{name}</h3>
+    {(tasks.length > 0) ?
     <div>
+         
       {tasks.map((task) => (
-        <div>{task.name}</div>
+        <Link to={`/task/${task.id}`} key={task.id}>
+          <div>{task.name}</div>
+        </Link>
       ))}
     </div>
+    : 
+    <div><h4>No tasks in this group</h4> </div>
+      }
+    <button onClick={() => createNewTask(id)}> Add new task</button>
   </div>
 );
 
@@ -20,4 +30,16 @@ const mapStateToProps = (state, ownProps) => {
     tasks: state.tasks.filter((task) => task.group === groupID),
   };
 };
-export const ConnectedTaskList = connect(mapStateToProps)(TaskList);
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    createNewTask(id) {
+      console.log("create new task ", id);
+      dispatch(requestTaskCreation(id));
+    },
+  };
+};
+export const ConnectedTaskList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TaskList);
